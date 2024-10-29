3d0022f78bd0c001b64c47feb2dbc4be0e4b25ee
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-10-29 21:12:54 +0530

    Add for loop examples in main.go

---
+++ 08ForLoop/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// while loop using for keyword
+
+	// i := 1
+
+	// for i <= 3 {
+	// 	fmt.Println(i)
+	// 	i++
+	// }
+
+	// infinite loop
+
+	// for {
+	// 	fmt.Println("1")
+	// }
+
+	// classic for loop
+
+	// for i:= 0; i < 3; i++{
+	// 	fmt.Println(i)
+	// }
+
+	// range
+
+	for i := range 3{
+		fmt.Println(i)
+	}
+}
