7081d372861e55268ef23f75a6bb8b67eedf458c
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-10-30 22:36:59 +0530

    Add slice examples in main.go

---
+++ 10Slices/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// var slice []int
+
+	// fmt.Println(slice)
+	// fmt.Println(len(slice))
+	// fmt.Println(slice == nil)
+
+	// var nums []int
+
+	// nums = make([]int,0,5)
+	// fmt.Println(len(nums))
+	// fmt.Println(cap(nums))
+	
+	// fmt.Println(nums[0])
+
+	// nums = append(nums, 1)
+	// nums = append(nums, 2)
+	// nums = append(nums, 3)
+	// nums = append(nums, 4)
+	// nums = append(nums, 5)
+	// fmt.Println(nums)
+	// fmt.Println(len(nums))
+	// fmt.Println(cap(nums))
+	// fmt.Println(nums)
+
+	// copy slices
+
+	// nums1 := make([]int,len(nums));
+	// fmt.Println(nums1)
+	// copy(nums1,nums)
+
+	// fmt.Println(nums1)
+
+
+	// slice
+
+	// var nums1 = []int{1,2,3,4,5}
+	// var nums2 = []int{1,2,3,4,5}
+
+	// fmt.Println(slices.Equal(nums1,nums2))
+	// a := nums[0:2]
+	// fmt.Println(a)
+
+	// b := nums[:3]
+	// fmt.Println(b)
+
+	// c := nums[1:]
+	// fmt.Println(c)
+
+	// slices package
+
+
+	var nums = [][]int{{1,2},{3,4}}
+	fmt.Println(nums)
+
+
+}
