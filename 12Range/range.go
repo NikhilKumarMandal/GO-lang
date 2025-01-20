a9c2d17af6999cd6cc3fe8e12c14b1cce3470dad
Nikhil <kumarnikhil48578@gmail.com>
2025-01-20 18:16:31 +0530

    Add range examples in range.go

---
+++ 12Range/range.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	//nums := []int{5, 6, 7}
+
+	// for i := 0; i < len(nums); i++ {
+	// 	fmt.Println(nums[i])
+	// }
+
+	// sum := 0
+	// i -> index
+	// num -> value
+	// for i,num := range nums {
+	// 	sum += num
+	// 	fmt.Println(i)
+	// }
+
+	// fmt.Println(sum)
+
+	// m := map[string]string{"fname": "john","lname":"doe"}
+
+	// for k,v := range m {
+	// 	fmt.Println(k,v)
+	// }
+
+	// for _,v := range m {
+	// 	fmt.Println(v)
+	// }
+
+	// unicode code point rune
+	// starting byte of rune
+	// 255  => 1 byte , if grater then 255 it will take more than 1 byte
+	for i,c := range "golang"{
+		fmt.Println(i,c)
+	}
+}
