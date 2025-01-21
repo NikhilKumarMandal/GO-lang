aee03a082a6e599dc786db64ac5361429c94d746
Nikhil <kumarnikhil48578@gmail.com>
2025-01-21 14:00:25 +0530

    Refactor main.go to demonstrate pointer usage and update changeNum function for reference semantics

--- 06mypointer/main.go
+++ 06mypointer/main.go
@@ -1,20 +1,46 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("Welcome to pointer class")
+
+// 	// var ptr *int
+// 	// fmt.Println("value of pointer is:", ptr)
+
+// 	myNumber := 25
+
+// 	var ptr = &myNumber
+// 	fmt.Println("value of actural pointer is",ptr)
+// 	fmt.Println("value of actural pointer is",*ptr)
+
+// 	*ptr = *ptr + 2
+// 	fmt.Println("New value is:",myNumber)
+
+// }
+
 package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Welcome to pointer class")
+// by value
+// func changeNum(num int){
+// 	num = 5
+// 	fmt.Println("In changeNum",num)
+// }
 
-	// var ptr *int
-	// fmt.Println("value of pointer is:", ptr)
+// by refrenece
+func changeNum(num *int){
+	*num = 5
+	fmt.Println("In changeNum",*num)
+}
 
-	myNumber := 25
+func main(){
+	num := 1
+	changeNum(&num)
+	// changeNum(num)
 
-	var ptr = &myNumber
-	fmt.Println("value of actural pointer is",ptr)
-	fmt.Println("value of actural pointer is",*ptr)
+	// fmt.Println("Memory Address",&num)
 
-	*ptr = *ptr + 2
-	fmt.Println("New value is:",myNumber)
-
+	fmt.Println("After changeNum in main func",num)
 }
